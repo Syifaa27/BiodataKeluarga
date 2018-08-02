a2892aa871bb35f8b838b2448fb15ec4efc1b2ae
user <user@DESKTOP-LUJ7RT6>
2018-08-02 11:22:32 +0700

    Tambah Input Bahan 1.

--- src/PraktekPercabangan2.java
+++ src/PraktekPercabangan2.java
@@ -4,10 +4,7 @@
  * and open the template in the editor.
  */
 
-/**
- *
- * @author user
- */
+import java.util.Scanner;
 public class PraktekPercabangan2 {
 
     /**
@@ -17,6 +14,12 @@ public class PraktekPercabangan2 {
         String identitas = "SYIFA' AULIYA ASH-SHOLIHAH / XR6 / 39";
         System.out.println("Identitas : "+ identitas);
         System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Bahan Pertama: ");
+        System.out.println("1. Pisang");
+        System.out.println("2. Telur");
+        System.out.print("Masukkan no pilihan Anda :");
+        int bahan1 = scanner.nextInt();
         
     }
     
