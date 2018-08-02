26cddd80455788aa4597862a591239bc88b589aa
user <user@DESKTOP-LUJ7RT6>
2018-08-02 11:14:39 +0700

    Initial Commit

---
+++ src/PraktekPercabangan2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class PraktekPercabangan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String identitas = "SYIFA' AULIYA ASH-SHOLIHAH / XR6 / 39";
+        System.out.println("Identitas : "+ identitas);
+        System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+        
+    }
+    
+}
