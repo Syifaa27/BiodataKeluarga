84e14312dcf87c6ce4470cbd2ab9e6e582b49f77
user <user@DESKTOP-LUJ7RT6>
2018-08-02 11:28:34 +0700

    Tambah Input Bahan 2.

--- src/PraktekPercabangan2.java
+++ src/PraktekPercabangan2.java
@@ -20,7 +20,22 @@ public class PraktekPercabangan2 {
         System.out.println("2. Telur");
         System.out.print("Masukkan no pilihan Anda :");
         int bahan1 = scanner.nextInt();
+        if(bahan == 1 || bahan1 == 2)
+        {
+            System.out.println("Bahan Kedua: ");
+            if(bahan1 == 1){
+                System.out.println("1. Susu");
+                System.out.println("2. Minyak Goreng");
+                System.out.println("3. Tidak ada");
+            }
+            System.out.print("Masukkan no pilihan Anda :");
+            int bahan2 = scanner.nextInt();
+        }else
+            System.out.println("Mohon Maaf, Pilihan tidak ditemukan, " + "tidak dapat memberikan saran resep");
         
+            }
+            
+        }
     }
     
 }
