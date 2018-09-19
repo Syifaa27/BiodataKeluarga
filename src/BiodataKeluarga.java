f4e260a375bd0429f422c9f04a6b70d514992aa5
user <user@DESKTOP-LUJ7RT6>
2018-09-19 14:21:00 +0700

    Commit Keseluruhan

---
+++ src/BiodataKeluarga.java
@@ -0,0 +1,32 @@
+
+/**
+ *
+ * @author SYIFA AULIYA
+ */
+public class BiodataKeluarga {
+    public String namaAyah;
+    public String namaIbu;
+    public String namaSaudara;
+    public String namaKamu;
+    public String alamat;
+    public String tempatLahir;
+    public String tanggalLahir;
+    public String hobi;
+    public String citaCita;
+    public String noTelepon;
+    
+    public void masukan() {
+        System.out.println("Nama Ayah       = "+ namaAyah);
+        System.out.println("Nama Ibu        = "+ namaIbu);
+        System.out.println("Nama Saudara    = "+ namaSaudara);
+        System.out.println("Nama Kamu       = "+ namaKamu);
+        System.out.println("Alamat          = "+ alamat);
+        System.out.println("Tempat Lahir    = "+ tempatLahir);
+        System.out.println("Tanggal Lahir   = "+ tanggalLahir);
+        System.out.println("Hobi            = "+ hobi);
+        System.out.println("Cita-cita       = "+ citaCita);
+        System.out.println("No Telepon      = "+ noTelepon);
+        
+        
+    }
+}
---
+++ src/BiodataKeluarga2.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author SYIFA AULIYA
+ */
+public class BiodataKeluarga2 {
+    public static void main(String[] args){
+        BiodataKeluarga abc = new BiodataKeluarga();
+        abc.namaAyah    = "Muhammad Hariyadi";
+        abc.namaIbu     = "Renny Lelyana";
+        abc.namaSaudara = "Muhammad Fahri Sauqi Majid";
+        abc.namaKamu    = "Syifa' Auliya Ash-Sholihah";
+        abc.alamat      = "Jl Danau Tambingan";
+        abc.tempatLahir = "Magetan";
+        abc.tanggalLahir= "15 Juli 2003";
+        abc.hobi        = "Menyanyi";
+        abc.citaCita    = "Dokter";
+        abc.noTelepon   = "08123252822";
+        abc.masukan();
+        
+    }
+    
+}
